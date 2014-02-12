67f1a59ef3eabf2a51c3e93575c056ccdbe7fb40
Jon Klein <jtklein@alaska.edu>
2014-02-12 14:31:41 -0900

    initial commit

---
+++ complex_lomb.py
@@ -0,0 +1,139 @@
+# using equations from http://www.ann-geophys.net/29/2101/2011/angeo-29-2101-2011.pdf
+# [1] ( A study of Traveling Ionospheric Disturbances and Atmospheric Gravity Waves using EISCAT Svalbard Radar IPY-data )
+
+# jon klein, jtklein@alaska.edu
+# 02/06/14
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+import pdb
+
+from scipy.optimize import curve_fit
+
+# calculate lomb peridogram for a complex signal with missing samples
+# samples at times t, for freqs 
+# len(freqs) should be at least 4 * len(t)
+def lomb(samples, t, freqs, normalized = True):
+    # check that freqs > 4N 
+    # check t is positive..
+    # check stuff and complain
+    return np.absolute(np.array([_p_w(samples, t, f * 2 * np.pi, normalized) for f in freqs]))
+
+# calculate significance of a power
+# equation 3 in [1]
+def lomb_ztop(z, freqs):
+    return 1-((1 - np.exp(-z)) ** len(freqs))
+
+def lomb_ptoz(p, freqs):
+    return -np.log(1 - ((1 - p) ** (-len(freqs))))
+
+
+# helper function for lomb peridogram
+# where t is numpy array of times, w is frequency in radians
+# equation 3 in [1]
+def _p_w(samples, t, w, normalized = True):
+    mean = np.mean(samples)
+
+    if normalized:
+        varscale = 1 / (2. * np.var(samples))
+
+    else:
+        varscale = 1
+
+    tau = sum(np.sin(2. * w * t)) / sum(np.cos(2. * w * t)) 
+ 
+    n1 = sum((samples - mean) * np.cos(w * (t - tau)))
+    n2 = sum((samples - mean) * np.sin(w * (t - tau)))
+    d1 = np.sqrt(sum(np.cos(w * (t - tau)) ** 2))
+    d2 = np.sqrt(sum(np.sin(w * (t - tau)) ** 2))
+    return varscale * ((abs(n1 / d1) ** 2) + (abs(n2 / d2) ** 2))
+
+# see SuperDARN Workshop, 26-31/05/2013, velocity error tutorial.. W / sqrt(Nlag) for now
+# calculate information about a peak
+# - power at lag zero for gaussian and lorentzian
+# - spectral width
+
+# the frequency response of a decaying exponential in the time domain
+# p * exp(-a * |x|)
+def lorentzian(freqs, a, p, df):
+    return p * ((2 * a)/(a ** 2 + 4 * (np.pi ** 2) * ((freqs-df) ** 2)))
+
+# the frequency reponse of a gaussian in the time domain
+# p * exp(-alpha * (x ** 2))
+def gaussian(freqs, alpha, p, df):
+    return p * np.sqrt(np.pi / alpha) * np.exp(-(((np.pi * (freqs-df))**2)/alpha))
+
+# lo - numpy array with spectrum
+# prob - significance array
+# freqs - numpy array of frequencies 
+
+# returns an array of indexes of input array
+# for items in array centered on centeridx which are True
+# this is probably not a pythonic approach.. see image segmentation?
+def get_segment(array, centeridx):
+    if not array[centeridx] == True:
+        raise ValueError('Specified center index of array in get_segment is False!')
+
+    segmentidxs = [centeridx]
+
+    _t = centeridx - 1
+    while array[_t] and _t >= 0:
+        segmentidxs = [_t] + segmentidxs
+        _t -= 1
+        
+
+    _t = centeridx + 1
+    while array[_t] and _t < len(array):
+        segmentidxs = segmentidxs + [_t]
+        _t += 1
+
+    return np.array(segmentidxs)
+
+if __name__ == '__main__':
+    f = 10
+    fs = 5000
+    t_total = .05
+    NOISE_SCALE = 2
+    
+
+    t = np.arange(0,t_total,1./fs)
+    noise = np.random.normal(scale=NOISE_SCALE,size=len(t)) + 1j * np.random.normal(scale=NOISE_SCALE,size=len(t))
+    
+    sin1 = 1 * np.exp(1j * 2 * np.pi * t * 100) * np.exp(-t * 10) 
+    sin4 = 1 * np.exp(1j * 2 * np.pi * t * 10) * np.exp(-t * 8)
+    samples =  sin1 + sin4+ noise
+    freqs = np.linspace(-fs/2,fs/2,len(t) * 4)
+    
+    lo = lomb(samples, t, freqs, normalized = True)
+    prob = lomb_ztop(lo, freqs)
+    #lo = 10 * lo * (2 * np.std(samples) / len(freqs))# denormalize data after computing probabilities
+
+    # plot results
+    plt.subplot(3,1,1)
+    plt.plot(t, np.real(samples))
+    plt.plot(t, np.imag(samples))
+    plt.xlabel('time (s)')
+    plt.ylabel('amplitude')
+    plt.title('time domain samples')
+    plt.legend(['I', 'Q'])
+    plt.grid(True)
+    
+    plt.subplot(3,1,2)
+    plt.plot(freqs, lo)
+    plt.xlabel('frequency (Hz)')
+    plt.ylabel('power')
+    plt.title('lomb periodogram')
+    plt.grid(True)
+    
+    plt.subplot(3,1,3)
+    plt.plot(freqs,prob) 
+    plt.xlabel('frequency (Hz)')
+    plt.ylabel('certainty')
+    plt.title('lomb periodogram certainty')
+    plt.grid(True)
+ 
+    plt.show()
+    pdb.set_trace()
+
+
---
+++ rawacf_to_fitlomb.py
@@ -0,0 +1,231 @@
+# jon klein, jtklein@alaska.edu
+# mit license
+import argparse
+import pdb
+import h5py
+
+from sd_data_tools import *
+from pydmap import DMapFile, timespan, dt2ts, ts2dt
+from complex_lomb import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.optimize import curve_fit
+import scipy.signal as signal
+
+I_OFFSET = 0
+Q_OFFSET = 1
+
+DECAY_IDX = 0
+POW_IDX = 1
+DF_IDX = 2
+
+class LombFit:
+    def __init__(self, record):
+        self.rawacf = record # dictionary copy of RawACF record
+        self.lags = range(record['mplgs']) # range of lags
+        self.nlags = record['mplgs'] # number of lags
+        self.ranges = range(record['nrang']) # range gates
+        self.nranges = record['nrang'] # range gates
+        self.pulses = record['ptab'] # (mppul length list): pulse table
+        self.npulses = record['mppul'] # number of pulses
+        self.ltab = record['ltab'] # (mplgs x 2 length list): lag table
+        self.t_first = self.rawacf['lagfr'] # lag to first range in us
+        self.t_pulse = self.rawacf['mpinc'] # multi pulse increment (tau, basic lag time) 
+        self.smsep = self.rawacf['smsep'] # sample separation in us
+        self.txpl = self.rawacf['txpl'] # sample separation in us
+        self.rsep = self.rawacf['rsep'] # range gate separation in km
+        self.acfd = self.record['acfd'] # acf values
+        
+        # TODO: copy over pwr0, ltab, ptab, slist, nlag
+        # TODO:
+        #       get widththreshold and peakthreshold.. 
+
+        self.freqs = linspace(0, 500, 100) # TODO: calculate frequencies in lomb spectrum
+        self.maxwidth = 20
+        self.widththreshold = .95 # widththreshold - statistical significance threshold for finding the extend of a peak
+        self.peakthreshold = .5 # peakthreshold - statistical significance threshold for finding returns
+
+        self.qflg = np.zeros(nranges)
+        self.gflg = np.zeros(nranges)
+
+        # initialize empty arrays for fitted parameters 
+        self.v          = [[] for r in range(nranges)]
+        self.v_e        = [[] for r in range(nranges)]
+ 
+        self.sd_s       = [[] for r in range(nranges)]
+        self.w_s_e      = [[] for r in range(nranges)]
+        self.w_s        = [[] for r in range(nranges)]
+        self.p_s        = [[] for r in range(nranges)]
+        self.p_s_e      = [[] for r in range(nranges)]
+        self.v_s        = [[] for r in range(nranges)]
+        self.v_s_e      = [[] for r in range(nranges)]
+
+        self.sd_l       = [[] for r in range(nranges)]
+        self.w_l_e      = [[] for r in range(nranges)]
+        self.w_l        = [[] for r in range(nranges)]
+        self.p_l        = [[] for r in range(nranges)]
+        self.p_l_e      = [[] for r in range(nranges)]
+        self.v_l        = [[] for r in range(nranges)]
+        self.v_l_e      = [[] for r in range(nranges)]
+
+        self.ProcessPulse()
+    
+    # writes out an hdf5 record of the lss fit
+    def WriteLSSFit(self):
+        pass         
+
+    # calculates FitACF-like parameters for each peak in the spectrum
+    # processes the pulse (move it __init__)?
+    def ProcessPulse(self):
+        for r in self.ranges:
+            peaks = ProcessPeaks(r)
+
+    # finds returns in spectrum and records cell velocity
+    def ProcessPeaks(self, rgate):
+        offset = rgate * self.nlags * 2
+        
+        # acfd is mplgs * nrang
+        # see http://davit.ece.vt.edu/davitpy/_modules/pydarn/sdio/radDataTypes.html
+        i_lags = np.array(self.acfd[offset + I_OFFSET : offset+self.nlags * 2 : 2])
+        q_lags = np.array(self.acfd[offset + Q_OFFSET : offset+self.nlags * 2 : 2])
+        
+        # TODO: FIND GOOD LAGS MASK
+        good_lags = np.ones(self.nlags)
+        
+        i_lags = i_lags[good_lags == True]
+        q_lags = q_lags[good_lags == True]
+        t = arange(0, self.nlags * self.t_pulse, self.t_pulse)[good_lags == True]
+        
+        samples = i_lags + 1j * q_lags
+
+        # calcuate lomb-scargle periodogram
+        lomb_range = lomb(samples, t, self.freqs)
+        prob_range = lomb_ztop(lo, self.freqs)
+
+        # find peaks (assuming "well separated" peaks...)
+        threshidxs = np.arange(len(self.freqs))[prob_range < self.peakthreshold]
+        peakidxs = signal.find_peaks_cwt(lomb_range, np.arange(1,self.maxwidth))
+        threshpeaksidxs = np.intersect1d(threshidxs, peakidxs)
+       
+        # calculate freq and prob 
+        threshpeakfreqs = self.freqs[threshpeaksidxs]
+        threshpeakprobs = prob_range[threshpeaksidxs]
+        threshwidth = prob_range < self.widththreshold
+
+        # calculate v, v_e
+        self.v[rgate] = threshpeakfreqs # TODO: apply doppler shift to calculate velocity (need TF?)
+        self.v_e[rgate] = threshpeakprobs # TODO: scale significance to some other error
+
+        # TODO: calculate qflg, gflg (ground and quality flags)
+        self.qflg[rgate] = 0
+        self.gflg[rgate] = 0
+
+        # unsupported: sd_phi
+         
+        for peakidx in peakidxs:
+            # extract peak
+            seg_idxs = get_segment(threshwidth, peakidx)
+            seg_freqs = self.freqs[seg_idxs] - self.freqs[peakidx]
+            seg_powers = lo[seg_idxs]
+
+            self.ProcessPeaks(rgate, peakidx, seg_idxs, seg_powers)
+
+    # processes a peak for sigma and lambda fit
+    def ProcessPeak(self, rgate, peakidx, seg_idxs, seg_powers):
+        # compute velocity, width, and power for each peak with "sigma" and "lambda" fits
+        # TODO: feed better initial fit parameters to curve_fit
+        # TODO: add exception checking to handle fit failures
+        # TODO: handle inf pcov (bad fits)
+        # TODO: detect horrible fits (nonphysical spectral widths)
+        # calculate "lambda" parameters
+        popt, pcov = curve_fit(lorentzian, seg_freqs, seg_powers, p0 = [1,5,0], maxfev=2000)
+
+        self.sd_l[rgate].append(pcov[DECAY_IDX][DECAY_IDX]) # TODO: what is sd_l (standard deviation of lambda?)
+
+        self.w_l[rgate].append(popt[DECAY_IDX])
+        self.w_l_e[rgate].append(pcov[DECAY_IDX][DECAY_IDX])
+
+        self.p_l[rgate].append(popt[POW_IDX])
+        self.p_l_e[rgate].append(pcov[POW_IDX][POW_IDX])
+
+        self.v_l[rgate].append(popt[DF_IDX] + freqs[peakidx])
+        self.v_l_e[rgate].append(pcov[DF_IDX][DF_IDX])
+
+        # calculate "sigma" parameters
+        popt, pcov = curve_fit(gaussian, seg_freqs, seg_powers, p0 = [1,5,0], maxfev=2000)
+
+        self.sd_l[rgate].append(pcov[DECAY_IDX][DECAY_IDX]) # TODO: what is sd_l (standard deviation of sigma?)
+
+        self.w_s[rgate].append(popt[DECAY_IDX])
+        self.w_s_e[rgate].append(pcov[DECAY_IDX][DECAY_IDX])
+
+        self.p_s[rgate].append(popt[POW_IDX])
+        self.p_s_e[rgate].append(pcov[POW_IDX][POW_IDX])
+
+        self.v_s[rgate].append(popt[DF_IDX] + freqs[peakidx])
+        self.v_s_e[rgate].append(pcov[DF_IDX][DF_IDX])
+
+    def PlotLomb(self):
+        # see http://matplotlib.org/examples/pylab_examples/pcolor_demo.html
+        x = self.freqs # dwvelocity/frequency 
+        y = self.ranges # ranges
+        z = 0 # certainty 
+        plt.pcolor(x, y, z, cmap='RdBu', vmin=z_min, vmax=z_max)
+        plt.axis([x.min(), x.max(), y.min(), y.max()])
+        plt.colorbar()
+        plt.show()
+
+    def CalcBadlags(drange):
+        # https://github.com/jspaleta/SuperDARN_MSI_ROS/blob/d925b7740e6a95c0e1b653a08bf8967361eb46f8/linux/home/radar/ros.3.6/codebase/superdarn/src.lib/tk/fitacf.2.5/src.lib/tk/fitacf.2.5/src/badlags.c
+        # see FitACFBadlags and FitACFCkRng
+        # https://web.archive.org/web/20110411212317/http://mediawiki.hmo.ac.za/mediawiki/index.php/FitACF
+        
+        # find crossrange interference
+
+        # find samples which overlap with transmitted pulses 
+        badsamples = [] 
+        badlags = []
+        rangesamp = (self.rsep * 20) / (3 * self.smsep) # calculate samples for each range gate
+        n_samples = max(self.pulses) * t_pulse / self.smsep + self.nranges * rangesamp  # may not cover all samples.. but enough for the pulse sequence
+        sample_times = [self.smsep * s for s in range(n_samples)]
+
+        for p in pulses: 
+            t1 = p * self.t_pulse - self.txpl/2.
+            t2 = t1 + 3 * self.txpl / 2. + 100
+
+            for ts in sample_times:
+                ts = r * smsep
+                
+                if ts >= t1 and ts <= t2:
+                    badsamples.append((ts - self.t_first)/self.smsep) # add bad sample index 
+
+        badsamples = sorted(list(set(badsamples))) # remove duplicates, and sort list of bad samples
+    
+        
+
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Processes RawACF files with a Lomb-Scargle periodogram to produce FitACF-like science data.')
+    
+    parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("--infile", help="input RawACF file to convert")
+    parser.add_argument("--outfile", help="output FitLSS file")
+    
+    args = parser.parse_args() 
+
+    infile = '20140204.2200.04.kod.c.rawacf'
+    dfile = DMapFile(files=[infile])
+
+    times = dfile.times
+    i = 0
+    for t in times:
+        i += 1
+        if i > 20:
+            pdb.set_trace()
+        fit = LombFit(dfile[t])
+
+
+    del dfile
+
