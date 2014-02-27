added error estimation for spectral width, velocity, and amplitude. fixed bugs in spectral width calculation

rawacf_to_fitlomb.py
@@ -29,10 +29,15 @@ DECAY_IDX = 0
 POW_IDX = 1
 DF_IDX = 2
 
-MAX_V = 1500 # m/s, max velocity to search for 
+FWHM_TO_SIGMA = 2.355 # conversion of fwhm to std deviation, assuming gaussian
+MAX_V = 1000 # m/s, max velocity (doppler shift) to include in lomb
+MAX_W = 2000 # m/s, max spectral width to include in lomb 
 C = 3e8
 
-VEL_CMAP = plt.cm.PuOr
+ALPHA_RES = 5 # m/s
+FREQ_RES = 5# m/s
+
+VEL_CMAP = plt.cm.RdBu
 FREQ_CMAP = plt.cm.spectral
 NOISE_CMAP = plt.cm.autumn
 SPECW_CMAP = plt.cm.hsv
@@ -61,29 +66,29 @@ class LombFit:
         self.recordtime = datetime.datetime(self.rawacf['time.yr'], self.rawacf['time.mo'], self.rawacf['time.dy'], self.rawacf['time.hr'], self.rawacf['time.mt'], self.rawacf['time.sc'], self.rawacf['time.us']) 
         
         # TODO: copy over pwr0, ltab, ptab, slist, nlag
-        # TODO:
-        #       get widththreshold and peakthreshold.. 
-            
+        
+        # calculate max decay rate for MAX_W spectral width
+        amax = (MAX_W) / (C / (self.tfreq * 1e3 * 2 * np.pi))
+
         # calculate max frequency either nyquist rate, or calculated off max velocity
         fmax = (MAX_V * 2 * (self.tfreq * 1e3)) / C
         nyquist = 1 / (2e-6 * self.t_pulse)
 
-        self.freqs = np.linspace(max(-nyquist, -fmax),min(nyquist, fmax), self.nlags * 10)
-        self.maxwidth = 20
-        self.widththreshold = .95 # widththreshold - statistical significance threshold for finding the extend of a peak
-        self.peakthreshold = .5 # peakthreshold - statistical significance threshold for finding returns
-        self.maxalf = 230
-        self.alfsteps = 150
+        self.freqs = np.linspace(max(-nyquist, -fmax),min(nyquist, fmax), self.nlags * 30)
+        
+        self.maxalf = amax
+        self.alfsteps = amax / ALPHA_RES
+        print 'alpha steps: ' + str(self.alfsteps')
         self.maxfreqs = 3
         self.alfs = np.linspace(0, self.maxalf, self.alfsteps)
 
         # thresholds on velocity and spectral width for surface scatter flag (m/s)
-        self.vss_thresh = 40
-        self.wss_thresh = 40
+        self.v_thresh = 30.
+        self.w_thresh = 90. # blanchard, 2009
         
-        # threshold on power (snr), spectral width fwhm, and velocity fwhm for quality flag
-        self.qwle_thresh = 15
-        self.qvle_thresh = 15
+        # threshold on power (snr), spectral width std error m/s, and velocity std error m/s for quality flag
+        self.qwle_thresh = 20
+        self.qvle_thresh = 20
         self.qpwr_thresh = 2
     
         # thresholds on velocity and spectral width for ionospheric scatter flag (m/s)
@@ -122,10 +127,8 @@ class LombFit:
         self.iflg       = np.zeros([self.nranges, self.maxfreqs])
         self.qflg       = np.zeros([self.nranges, self.maxfreqs])
         
-        self.v_l_e      = np.zeros([self.nranges, self.maxfreqs])
-
         self.CalcBadlags()
-
+    
     # writes out a record of the lss fit
     def WriteLSSFit(self):
         pass         
@@ -138,10 +141,10 @@ class LombFit:
         self.ProcessPeaks()
     
     # TODO: work with non-lambda env models
-    def ParallelProcessPulse(self):
+    def ParallelProcessPulse(self, cubecache = False):
         # create pp job server
         job_server = pp.Server()#ppservers=("137.229.27.61",""))
-
+        
         # prepare sample and time arrays 
         times_samples = [(self._CalcSamples(r)) for r in self.ranges]
         
@@ -149,22 +152,20 @@ class LombFit:
         
         # dispatch jobs
         libs = ("numpy as np", "numexpr as ne", "timecube", "iterative_bayes")
-        funcs = (make_spacecube, find_fwhm, calculate_bayes)
+        funcs = (make_spacecube, TimeCube, find_fwhm, calculate_bayes)
 
         for r in self.ranges:
-            args = (times_samples[r][1], times_samples[r][0], self.freqs, self.alfs, 1, self.maxfreqs)
+            args = (times_samples[r][1], times_samples[r][0], self.freqs, self.alfs, 1, self.maxfreqs, cubecache)
             jobs.append(job_server.submit(iterative_bayes, args, funcs, libs))
         
         # wait for jobs to complete
         job_server.wait() 
         
         for (rgate, job) in enumerate(jobs):
-            try:
-                self.lfits[rgate] = job()
-            except:
-                pdb.set_trace()
-        job_server.print_stats()
-        job_server.destroy()         
+            self.lfits[rgate] = job()
+
+        job_server.destroy() 
+
         self.ProcessPeaks()
 
     # get time and good complex samples for a range gate
@@ -198,7 +199,7 @@ class LombFit:
         # calcuate generalized lomb-scargle periodogram iteratively
         self.lfits[rgate] = iterative_bayes(samples, t, self.freqs, self.alfs, env_model, self.maxfreqs, cubecache = cubecache)
         #self.sfits[rgate] = iterative_bayes(samples, t, freqs, alfs, maxfreqs = 2, env_model = 2)
-        
+
     def ProcessPeaks(self):
         # compute velocity, width, and power for each peak with "sigma" and "lambda" fits
         # TODO: add exception checking to handle fit failures
@@ -206,27 +207,35 @@ class LombFit:
             for (i, fit) in enumerate(self.lfits[rgate]):
                 # calculate "lambda" parameters
                 np.append(self.sd_l[rgate],0) # TODO: what is sd_l (standard deviation of lambda?)
-                
+                 
                 # see Effects of mixed scatter on SuperDARN convection maps near (1) for spectral width 
-                self.w_l[rgate,i] = C / (2 * np.pi * fit['alpha'] * self.tfreq)
-                self.w_l_e[rgate,i] = fit['alpha_fwhm']
+                self.w_l[rgate,i] = (fit['alpha'] / (2 * np.pi)) * (C / (self.tfreq * 1e3))
+                dalpha = self.alphas[1] - self.alphas[0]
+                # approximate alpha error by taking half of range of alphas covered in fwhm
+                self.w_l_e[rgate,i] = fit['alpha_fwhm'] * dalpha / FWHM_TO_SIGMA
+                
+                # amplitude estimation, see bayesian analysis v: amplitude estimation, multiple well separated sinusoids
+                # bretthorst, equation 78
                 self.p_l[rgate,i] = fit['amplitude'] / self.noise
-                self.p_l_e[rgate,i] = 0
+                pdb.set_trace()
+                self.p_l_e[rgate,i] = np.sqrt(self.noise/fit['amplitude_error_unscaled'])/self.noise
+
                 v_l = (fit['frequency'] * C) / (2 * self.tfreq * 1e3)
                 self.v_l[rgate,i] = v_l
-                self.v_l_e[rgate,i] = fit['frequency_fwhm']
+                # approximate velocity error as half the range of velocities covered by fwhm 
+                # "nonuniform sampling: bandwidth and aliasing", page 25
+                _df = self.freqs[1] - self.freqs[0]
+                self.v_l_e[rgate,i] = (((fit['frequency_fwhm'] * _df) * C) / (2 * self.tfreq * 1e3)) / FWHM_TO_SIGMA
                 
-                # set gflg if any of the returns match vss_thresh and wss_thresh thresholds
-                # ss - surface scatter, v - velocity, w - spectral width
-                if v_l < self.vss_thresh and self.w_l[rgate, i] < self.wss_thresh:
+                # for information on setting surface/ionospheric scatter thresholds, see
+                # A new approach for identifying ionospheric backscatterin midlatitude SuperDARN HF radar observations
+                if abs(v_l) - (self.v_thresh - (self.v_thresh / self.w_thresh) * abs(self.w_l[rgate, i])) > 0: 
+                    self.iflg[rgate,i] = 1
+                else:
                     self.gflg[rgate,i] = 1
                 
-                # set iflg if ionospheric scatter velocity and spectral width thresholds are met
-                if v_l < self.vimax_thresh and v_l > self.vimin_thresh and self.w_l[rgate,i] > self.wimin_thresh and self.w_l[rgate, i] < self.wimax_thresh:
-                    self.iflg[rgate,i] = 1
-
                 # set qflg if .. signal to noise ratios are high enough, not stuck 
-                if self.p_l[rgate,i] > self.qpwr_thresh and self.w_l_e[rgate,i] < self.qwle_thresh and self.w_l_e[rgate, i] < self.qvle_thresh:
+                if self.p_l[rgate,i] > self.qpwr_thresh and self.w_l_e[rgate,i] < self.qwle_thresh and self.v_l_e[rgate, i] < self.qvle_thresh:
                     self.qflg[rgate,i] = 1
 
             '''
@@ -276,6 +285,16 @@ class LombFit:
         
         self.bad_lags = bad_lags
 
+
+def PlotMixed(lomb):
+    ionospheric_scatter = np.sum(lomb.iflg * lomb.qflg, axis=1) > 0
+    surface_scatter = np.sum(lomb.gflg * lomb.qflg, axis=1) > 0
+    mixed_scatter = (ionospheric_scatter * surface_scatter).nonzero()[0]
+    if len(mixed_scatter):
+        for mixed in mixed_scatter:
+            lomb.PlotPeak(mixed)
+
+
 # replicate rti-style plot 
 # plot w_l, p_l, and v_ms of main peak as a function of time
 # adapted from jef's plot-rti.py
@@ -283,31 +302,21 @@ def PlotRTI(lombfits, beam):
     # assemble pulse time list
     times = [fit.recordtime for fit in lombfits]
     maxfreqs = lombfits[0].maxfreqs
-
-    velocity = np.zeros([len(times), lombfits[0].nranges])
+    ranges = [lf.nranges for lf in lombfits]
+    velocity = np.zeros([len(times), max(ranges)])
     for i in range(maxfreqs):
         plt.subplot(maxfreqs, 1, i+1)
         # assemble velocity list, collect velocity at beam number
         for (t,pulse) in enumerate(lombfits):
             if pulse.bmnum != beam:
                 continue
-            velocity[t,:] = pulse.v_l[:,i] * pulse.qflg[:,i] 
+            velocity[t,:] = np.concatenate((pulse.v_l[:,i] * pulse.qflg[:,i], np.zeros(max(ranges) - pulse.nranges)))
 
-            ionospheric_scatter = np.sum(pulse.iflg * pulse.qflg, axis=1) > 0
-            surface_scatter = np.sum(pulse.gflg * pulse.qflg, axis=1) > 0
-            mixed_scatter = ionospheric_scatter * surface_scatter
-             
-            if sum(mixed_scatter):
-                print 'mixed scatter at: ' + str( mixed_scatter.nonzero())
-
-
-            
-        
         x = dates.date2num(times)
-        y = np.array(lombfits[0].ranges)
+        y = np.array(np.arange(max(ranges)))
         
         plt.pcolor(x, y, velocity.T, cmap = VEL_CMAP)
-
+        plt.clim([-500,500])
         plt.axis([x.min(), x.max(), y.min(), y.max()])
         #plt.axis([x.min(), x.max(), y.min(), y.max()])
         plt.grid(True)
@@ -321,24 +330,9 @@ def PlotRTI(lombfits, beam):
         ax.xaxis.set_major_formatter(dateformatter)
     plt.show()
 
-    pdb.set_trace()
-    #fig = plt.gcf() 
-    #fig.autofmt_xdate()
-
-        
-    
-    # prepare     
-
-        # see http://matplotlib.org/examples/pylab_examples/pcolor_demo.html
-        #x = self.freqs # dwvelocity/frequency 
-        #y = self.ranges # ranges
-        #z = 0 # certainty 
-        #plt.pcolor(x, y, z, cmap='RdBu', vmin=z_min, vmax=z_max)
-        #plt.axis([x.min(), x.max(), y.min(), y.max()])
-        #plt.colorbar()
-        #plt.show()
 
 if __name__ == '__main__':
+    import pickle
     parser = argparse.ArgumentParser(description='Processes RawACF files with a Lomb-Scargle periodogram to produce FitACF-like science data.')
     
     parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
@@ -348,7 +342,7 @@ if __name__ == '__main__':
     args = parser.parse_args() 
 
     # good time at McM is 3/20/2013, 8 AM UTC
-    infile = '/mnt/flash/sddata/0208/all.rawacf'
+    infile = '/mnt/windata/sddata/0207/all.rawacf'
     #20130320.0801.00.mcm.a.rawacf'#'20130320.0801.00.mcm.a.rawacf'
     dfile = DMapFile(files=[infile])
 
@@ -359,11 +353,19 @@ if __name__ == '__main__':
     for (i,t) in enumerate(times):
         if(dfile[t]['bmnum'] != 9):
             continue
+        if i < 3000:
+            continue
+        if i > 12000:
+            break
+        
+
         print i
         print 'processing time ' + str(t)
         fit = LombFit(dfile[t])
-
+        
         fit.ParallelProcessPulse()
         lombfits.append(fit)
-    PlotRTI(lombfits, 9)
+        pickle.dump(lombfits, open('beam9_ade_0207.re.p', 'wb'))
+    del dfile
+    #PlotRTI(lombfits, 9)
     