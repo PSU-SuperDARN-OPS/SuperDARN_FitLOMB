fixed bug in lomb calculation, work on plotting data

iterative_bayes.py
@@ -3,9 +3,7 @@
 
 # equations to use bayesian analysis to determine the spectral content of a FitACF return
 # references:
-# (1)
-# (2) 
-# (3) "Bayesian Analysis. III. Applications to NMR Signal Detection, Model Selection and Parameter Estimation" by  G. Larry Bretthorst, 1990
+# "Bayesian Analysis. III. Applications to NMR Signal Detection, Model Selection and Parameter Estimation" by  G. Larry Bretthorst, 1990
 
 # TODO: add better measures of fit certainty
 
@@ -17,8 +15,8 @@ import pdb
 from timecube import TimeCube, make_spacecube
 VERBOSE = False 
 
-# jef spaleta's code..
-# modified variable "half" factor (for working with logs)
+# modified from jef spaleta's code..
+# -added variable "half" factor (for working with logs)
 # may be better to fit a spline
 # (see http://stackoverflow.com/questions/10582795/finding-the-full-width-half-maximum-of-a-peak)
 # however, this would assume that a spline is a good model, that approach may break with multiple peaks
@@ -116,8 +114,8 @@ def calculate_bayes(s, t, f, alfs, env_model, cubecache = False, timecube = Fals
     # omegas * times * alphas
     # TODO: [12] has real - imag, but jef has real + imag. only jef's way works.. why?
     # about 50% of execution time is spent here
-    R_f = (np.dot(np.real(s), ce_matrix) + np.dot(np.imag(s), se_matrix)).T
-    I_f = (np.dot(np.real(s), se_matrix) - np.dot(np.real(s), ce_matrix)).T
+    R_f = (np.dot(np.real(s), ce_matrix) - np.dot(np.imag(s), se_matrix)).T
+    I_f = (np.dot(np.real(s), se_matrix) + np.dot(np.imag(s), ce_matrix)).T
     
     # we might be able to eliminate constants.. considering that we blow them away with the normalization anyways
     # hbar2 is a "sufficient statistic" 
@@ -132,11 +130,11 @@ def calculate_bayes(s, t, f, alfs, env_model, cubecache = False, timecube = Fals
     # don't bother de-logging, we don't use this anyways.
     #P_f = 10 * pow(10., P_f)
     #P_f = ((N * dbar2 - hbar2) ** ((2 - N) / 2.)) / C_f # (18) in [4]
-    #P_f /= P_f.sum() # this is probably not valid..
+    # P_f /= P_f.sum() # this is probably not valid..
     
     # see "Nonuniform Sampling: Bandwidth and Aliasing"
     # for <sigma**2>
-    #sigma2 = (N * dbar2 - hbar2) / (N - 4.) # ???
+    # sigma2 = (N * dbar2 - hbar2) / (N - 4.) # ???
 
     maxidx = np.argmax(P_f)
     max_tuple = np.unravel_index(maxidx, P_f.shape)
@@ -164,26 +162,4 @@ def calculate_bayes(s, t, f, alfs, env_model, cubecache = False, timecube = Fals
     # B_est from  I_est[max] / S_est[max] (what is it?)
 
 if __name__ == '__main__':
-    fs = 1. 
-    ts = 1./fs
-    t_total = 20 * (1/fs)
-    nfreqs_mult = 10
-    NOISE_SCALE = .5 
-    MAX_SIGNALS = 5
-    alfs = np.linspace(0,1,50) # range of possible decay rates
-    t = np.arange(0,t_total,1./fs)
-
-    nfreqs = nfreqs_mult * len(t) 
-    freqs = np.linspace(-.5/ts, .5/ts, nfreqs)
-
-    noise = np.random.normal(scale=NOISE_SCALE,size=len(t)) + 1j * np.random.normal(scale=NOISE_SCALE,size=len(t))
-    sin1 = 1 * np.exp(1j * 2 * np.pi * t * -.25) * np.exp(-t *.10) 
-    sin2 = 2 * np.exp(1j * 2 * np.pi * t * .07) * np.exp(-t * .02)
-
-    samples =  sin1 + sin2 + noise
-
-    for si in range(MAX_SIGNALS):
-        fit = calculate_bayes(samples, t, freqs, alfs)
-        fit['index'] = si
-        samples -= fit['amplitude'] * np.exp(1j * 2 * np.pi * t * fit['frequency']) * np.exp(-t * fit['alpha'])
-    
+    pass 

rawacf_to_fitlomb.py
@@ -34,13 +34,13 @@ MAX_V = 1000 # m/s, max velocity (doppler shift) to include in lomb
 MAX_W = 2000 # m/s, max spectral width to include in lomb 
 C = 3e8
 
-ALPHA_RES = 5 # m/s
-FREQ_RES = 5# m/s
+ALPHA_RES = 1 # m/s
+FREQ_RES = 1 # m/s
 
 VEL_CMAP = plt.cm.RdBu
 FREQ_CMAP = plt.cm.spectral
 NOISE_CMAP = plt.cm.autumn
-SPECW_CMAP = plt.cm.hsv
+SPECW_CMAP = plt.cm.hot
 POWER_CMAP = plt.cm.jet
 
 class LombFit:
@@ -77,8 +77,7 @@ class LombFit:
         self.freqs = np.linspace(max(-nyquist, -fmax),min(nyquist, fmax), self.nlags * 30)
         
         self.maxalf = amax
-        self.alfsteps = amax / ALPHA_RES
-        print 'alpha steps: ' + str(self.alfsteps')
+        self.alfsteps = int(amax / ALPHA_RES)
         self.maxfreqs = 3
         self.alfs = np.linspace(0, self.maxalf, self.alfsteps)
 
@@ -209,16 +208,17 @@ class LombFit:
                 np.append(self.sd_l[rgate],0) # TODO: what is sd_l (standard deviation of lambda?)
                  
                 # see Effects of mixed scatter on SuperDARN convection maps near (1) for spectral width 
-                self.w_l[rgate,i] = (fit['alpha'] / (2 * np.pi)) * (C / (self.tfreq * 1e3))
-                dalpha = self.alphas[1] - self.alphas[0]
+                self.w_l[rgate,i] = (C * fit['alpha']) / (2. * np.pi * (self.t_pulse * 1e-6) * (self.tfreq * 1e3)) 
+            
+                dalpha = self.alfs[1] - self.alfs[0]
                 # approximate alpha error by taking half of range of alphas covered in fwhm
                 self.w_l_e[rgate,i] = fit['alpha_fwhm'] * dalpha / FWHM_TO_SIGMA
                 
                 # amplitude estimation, see bayesian analysis v: amplitude estimation, multiple well separated sinusoids
-                # bretthorst, equation 78
+                # bretthorst, equation 78, I'm probably doing this wrong...
+                # to match fitacf, scale p_l by 10 * log10
                 self.p_l[rgate,i] = fit['amplitude'] / self.noise
-                pdb.set_trace()
-                self.p_l_e[rgate,i] = np.sqrt(self.noise/fit['amplitude_error_unscaled'])/self.noise
+                self.p_l_e[rgate,i] = np.sqrt(self.noise ** 2/fit['amplitude_error_unscaled'])/self.noise # this may be scaled wrong..
 
                 v_l = (fit['frequency'] * C) / (2 * self.tfreq * 1e3)
                 self.v_l[rgate,i] = v_l
@@ -298,7 +298,7 @@ def PlotMixed(lomb):
 # replicate rti-style plot 
 # plot w_l, p_l, and v_ms of main peak as a function of time
 # adapted from jef's plot-rti.py
-def PlotRTI(lombfits, beam):
+def PlotVRTI(lombfits, beam):
     # assemble pulse time list
     times = [fit.recordtime for fit in lombfits]
     maxfreqs = lombfits[0].maxfreqs
@@ -316,7 +316,40 @@ def PlotRTI(lombfits, beam):
         y = np.array(np.arange(max(ranges)))
         
         plt.pcolor(x, y, velocity.T, cmap = VEL_CMAP)
-        plt.clim([-500,500])
+        plt.clim([-800,800])
+        plt.axis([x.min(), x.max(), y.min(), y.max()])
+        #plt.axis([x.min(), x.max(), y.min(), y.max()])
+        plt.grid(True)
+        plt.colorbar()
+        ax = plt.gca()
+
+        locator = dates.AutoDateLocator()
+        dateformatter = dates.AutoDateFormatter(locator)
+
+        ax.xaxis.set_major_locator(locator) 
+        ax.xaxis.set_major_formatter(dateformatter)
+    plt.show()
+
+
+def PlotWRTI(lombfits, beam):
+    # assemble pulse time list
+    times = [fit.recordtime for fit in lombfits]
+    maxfreqs = lombfits[0].maxfreqs
+    ranges = [lf.nranges for lf in lombfits]
+    widths = np.zeros([len(times), max(ranges)])
+    for i in range(maxfreqs):
+        plt.subplot(maxfreqs, 1, i+1)
+        # assemble velocity list, collect velocity at beam number
+        for (t,pulse) in enumerate(lombfits):
+            if pulse.bmnum != beam:
+                continue
+            widths[t,:] = np.concatenate((pulse.w_l[:,i] * pulse.qflg[:,i], np.zeros(max(ranges) - pulse.nranges)))
+
+        x = dates.date2num(times)
+        y = np.array(np.arange(max(ranges)))
+        
+        plt.pcolor(x, y, widths.T, cmap = SPECW_CMAP)
+        plt.clim([0,1500])
         plt.axis([x.min(), x.max(), y.min(), y.max()])
         #plt.axis([x.min(), x.max(), y.min(), y.max()])
         plt.grid(True)
@@ -342,8 +375,8 @@ if __name__ == '__main__':
     args = parser.parse_args() 
 
     # good time at McM is 3/20/2013, 8 AM UTC
-    infile = '/mnt/windata/sddata/0207/all.rawacf'
-    #20130320.0801.00.mcm.a.rawacf'#'20130320.0801.00.mcm.a.rawacf'
+    #infile = '/mnt/windata/sddata/0207/all.rawacf'
+    infile = '20130320.0801.00.mcm.a.rawacf' #20130320.0801.00.mcm.a.rawacf'#'20130320.0801.00.mcm.a.rawacf'
     dfile = DMapFile(files=[infile])
 
     times = dfile.times
@@ -353,19 +386,15 @@ if __name__ == '__main__':
     for (i,t) in enumerate(times):
         if(dfile[t]['bmnum'] != 9):
             continue
-        if i < 3000:
-            continue
-        if i > 12000:
+        if i > 100:
             break
-        
-
         print i
         print 'processing time ' + str(t)
         fit = LombFit(dfile[t])
         
         fit.ParallelProcessPulse()
         lombfits.append(fit)
-        pickle.dump(lombfits, open('beam9_ade_0207.re.p', 'wb'))
+        pickle.dump(lombfits, open('beam9_mcm.p', 'wb'))
     del dfile
     #PlotRTI(lombfits, 9)
     