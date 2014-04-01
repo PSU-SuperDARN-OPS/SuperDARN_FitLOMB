fixed bug in w_l_e scaling

fitlomb_tools.py
@@ -76,10 +76,6 @@ def createMergefile(radar, day, datadir):
         for beam in f:
             for pulse in f[beam]:
                 dset = beam + '/' + pulse 
-                pdb.set_trace()
-                f[dset].attrs['nrang'] = f[beam].attrs['nrang']
-                f[dset].attrs['frang'] = f[beam].attrs['frang']
-                f[dset].attrs['rsep'] = f[beam].attrs['rsep']
                 mergefile[dset] = h5py.ExternalLink(h5f.split('/')[-1], dset)
 
 
@@ -102,8 +98,19 @@ def PlotRTI(times, ranges, z, cmap, lim):
         plt.subplot(MAX_LOMBDEPTH, 1, i+1)
         x = dates.date2num(times) 
 
-        y = ranges[0]
+        maxranges = ranges[np.argmax([len(r) for r in ranges])]
+        y = maxranges
 
+        # replicate data for plotting if number of range gates is lower than maximum 
+        # this assumes frang doesn't change
+        for (j,r) in enumerate(ranges):
+            repfactor = int(len(maxranges) / len(r))
+            ztemp = (z[j][:len(r)]).copy()
+
+            for k in range(len(maxranges)):
+                z[j][k] = ztemp[k/repfactor]
+        # so, number of range gates varies from 75 to 225
+        # need to fit into array of size 255...
         plt.pcolor(x, y, z[:,:,i].T, cmap = cmap)
         plt.axis([x.min(), x.max(), y.min(), y.max()])
 

rawacf_to_fitlomb.py
@@ -297,7 +297,7 @@ class LombFit:
                 self.w_l[rgate,i] = (C * fit['alpha']) / (2. * np.pi * (self.tfreq * 1e3)) 
 
                 # approximate alpha error by taking half of range of alphas covered in fwhm
-                self.w_l_e[rgate,i] = fit['alpha_fwhm'] / FWHM_TO_SIGMA
+                self.w_l_e[rgate,i] = ((C * fit['alpha_fwhm']) / (2. np.pi * (self.tfreq * 1e3))) / FWHM_TO_SIGMA
                 
                 # amplitude estimation, see bayesian analysis v: amplitude estimation, multiple well separated sinusoids
                 # bretthorst, equation 78, I'm probably doing this wrong...