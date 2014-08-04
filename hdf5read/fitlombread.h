2aacd0382c5d479b4ba9d5a4c4f3d132b3fc12b0
Jon Klein <jtklein@alaska.edu>
2014-08-04 15:12:13 -0800

    added pulse table, lag table, and origin time to c fitlomb reader

---
+++ hdf5read/fitlombread.h
@@ -0,0 +1,19 @@
+struct LombFile {
+    hid_t file_id;
+    hid_t root_group;
+    herr_t status; /* status of last HDF5 command */
+    hsize_t npulses; /* number of pulses in file */
+    hsize_t pulseidx; /* index of current pulse in array */
+};
+
+
+int32_t LombFitOpen(struct LombFile *lombfile, char *filename);
+int32_t LombFitClose(struct LombFile *lombfile);
+herr_t LombFitReadAttr(struct LombFile *lombfile, char *groupname, char *attrname, void *attrdata);
+void * LombFitReadVector(hid_t recordgroup, char *dsetname);
+int LombFitRead(struct LombFile *lombfile, struct RadarParm *rprm, struct FitData *fit);
+int LombFitSeek(struct LombFile *lombfile, int yr,int mo,int dy,int hr,int mt,int sc,double *atme);
+
+
+
+
