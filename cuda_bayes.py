51db7ce30973236506c287b48ab492fd49e505b1
Jon Klein <jtklein@alaska.edu>
2014-09-16 17:06:37 -0800

    fixed indexing of shared memory sample buffer in cuda

--- cuda_bayes.py
+++ cuda_bayes.py
@@ -22,56 +22,53 @@ mod = pycuda.compiler.SourceModule("""
 #define REAL 0
 #define IMAG 1
 
-
-// spawn one calc_bayes per frequency.., this makes it easier to parallelize dot products
-// then, condense down to lag calculations for other stuff
-// assuming...
-// cs_matrix is [freq][alpha][time]
-// TODO: check that the ce matrix is stored how I think it is..
-// RI - range * freq * alpha 
-
 __global__ void calc_bayes(float *samples, float *lags, float *ce_matrix, float *se_matrix, float *cs_f, float *R_f, float *I_f, double *hbar2, double *P_f, float env_model, uint32_t nsamples, uint32_t nalphas, float dbar2)
 {
     // possible optimizations:
     // - eliminate cs_f freq dependence..
-    uint16_t t, i;
-    __shared__ float s_samples[100];
-    uint32_t samplebase = blockIdx.x * nsamples * 2; 
-    uint32_t RI_offset = (blockIdx.x * blockDim.x * nalphas) + (threadIdx.x * nalphas);
+    uint16_t t, i, sample_offset, CS_offset;
 
     // cache samples in shared memory
+    __shared__ float s_samples[100];
+    uint32_t samplebase = blockIdx.x * nsamples * 2; 
+
     for(i = 0; i < 2 * nsamples / blockDim.x + 1; i++) {
-        uint32_t sample_offset = threadIdx.x + i * blockDim.x;
+        sample_offset = threadIdx.x + i * blockDim.x;
+
         if(sample_offset < nsamples * 2) {
-            s_samples[samplebase + threadIdx.x + i * blockDim.x] = samples[samplebase + threadIdx.x + i * blockDim.x];
+            s_samples[sample_offset] = samples[samplebase + sample_offset];
+        
         }
     }
 
+    // 90% of time spent on this loop..
     for(i =  0; i < nalphas; i++) {
-        uint32_t CS_offset = (threadIdx.x * nalphas * nsamples) + (i * nsamples);
-        R_f[RI_offset + i] = 0;
-        I_f[RI_offset + i] = 0;
-        
+        uint32_t RI_offset = (blockIdx.x * blockDim.x * nalphas) + (i * blockDim.x) + threadIdx.x;
+        R_f[RI_offset] = 0;
+        I_f[RI_offset] = 0;
+
         // TODO: if T is a good lag..
-    
-        // 90% of time spent on this loop..
-        // sample access is not a big deal..
+        // TODO: reduce stride in ce/se matrix, change to [alpha][time][freq]
+        // ~80% of time here
         for(t = 0; t < nsamples; t++) {
-            // reorganize memory to access adjacent samples?
-            uint32_t sample_offset = samplebase + 2*t;
-            R_f[RI_offset + i] +=   s_samples[sample_offset + REAL] * ce_matrix[CS_offset + t] + \
-                                    s_samples[sample_offset + IMAG] * se_matrix[CS_offset + t];
-            I_f[RI_offset + i] +=   s_samples[sample_offset + REAL] * se_matrix[CS_offset + t] - \
-                                    s_samples[sample_offset + IMAG] * ce_matrix[CS_offset + t];
+            CS_offset = (i * blockDim.x * nsamples) + (t * blockDim.x);
+            sample_offset = 2*t;
+
+            R_f[RI_offset] +=   s_samples[sample_offset + REAL] * ce_matrix[CS_offset + t] + \
+                                s_samples[sample_offset + IMAG] * se_matrix[CS_offset + t];
+            I_f[RI_offset] +=   s_samples[sample_offset + REAL] * se_matrix[CS_offset + t] - \
+                                s_samples[sample_offset + IMAG] * ce_matrix[CS_offset + t];
         }
 
-        hbar2[RI_offset + i] = (pow(R_f[RI_offset + i],2) / cs_f[CS_offset + i] + \
-                                pow(I_f[RI_offset + i],2) / cs_f[CS_offset + i]) / 2;
+        // ~20% of time
+        hbar2[RI_offset] = (pow(R_f[RI_offset],2) / cs_f[i] + \
+                            pow(I_f[RI_offset],2) / cs_f[i]) / 2;
 
-        P_f[RI_offset + i] = log10(nsamples * 2 * dbar2 - hbar2[RI_offset + i]) * (1 - nsamples) - log10(cs_f[CS_offset + i]);
+        P_f[RI_offset] = log10(nsamples * 2 * dbar2 - hbar2[RI_offset + i]) * (1 - nsamples) - log10(cs_f[i]);
         
     }
     __syncthreads();
+    // TODO: reverify samples, verify hbar2/P_f
 }
 """)
 
@@ -102,9 +99,10 @@ def main():
     amp = [10.]
     alf = [10.]
     env_model = 1
-    lags = np.arange(0, 20) * ts
+    lags = np.arange(0, 25) * ts
     times=[]
     signal=[]
+    CUDA_GRID = 75 
 
     for i in xrange(num_records):
       F=f[0]+0.1*np.random.randn()+float(i-num_records/2)/float(num_records)*.1*f[0]
@@ -119,10 +117,13 @@ def main():
         sig=A * np.exp(1j * 2 * np.pi * F * lag) * np.exp(-U * lag)+N_A*np.exp(1j*N_phase)
         signal.append(sig)
    
-    samples=np.float32(list(chain.from_iterable(izip(np.real(signal), np.imag(signal)))))
-    times=np.array(np.float32(times))
-    freqs = np.linspace(-fs/2, fs/2, 200)
+    samples=np.tile(np.float32(list(chain.from_iterable(izip(np.real(signal), np.imag(signal))))), CUDA_GRID)
+    freqs = np.linspace(-fs/2, fs/2, 100)
     alfs = np.linspace(0,fs/2., 60)
+
+    times=np.array(np.float32(times))
+    ce_matrix_cpu, se_matrix_cpu, CS_f_cpu = make_spacecube(times, freqs, alfs, env_model)
+    times=np.tile(np.array(np.float32(times)), CUDA_GRID)
     ce_matrix, se_matrix, CS_f = make_spacecube(times, freqs, alfs, env_model)
     # TODO: RESHAPE CE/SE MATRIX FOR FREQ/ALPHA/TIME (swap 2,3?)
     # 50 50 20 (
@@ -131,10 +132,10 @@ def main():
     CS_f_g = np.float32(np.swapaxes(CS_f,0,1).flatten())
     CS_f = np.float32(CS_f)
 
-    R_f = np.float32(np.zeros([len(freqs), len(alfs)]))
-    I_f = np.float32(np.zeros([len(freqs), len(alfs)]))
-    hbar2 = np.float64(np.zeros([len(freqs), len(alfs)]))
-    P_f = np.float64(np.zeros([len(freqs), len(alfs)]))
+    R_f = np.float32(np.zeros([CUDA_GRID, len(freqs), len(alfs)]))
+    I_f = np.float32(np.zeros([CUDA_GRID, len(freqs), len(alfs)]))
+    hbar2 = np.float64(np.zeros([CUDA_GRID, len(freqs), len(alfs)]))
+    P_f = np.float64(np.zeros([CUDA_GRID, len(freqs), len(alfs)]))
 
     # allocate space on GPU 
     samples_gpu = cuda.mem_alloc(samples.nbytes)
@@ -142,10 +143,10 @@ def main():
     ce_gpu = cuda.mem_alloc(ce_matrix_g.nbytes)
     se_gpu = cuda.mem_alloc(se_matrix_g.nbytes)
     CS_f_gpu = cuda.mem_alloc(CS_f.nbytes)
-    R_f_gpu = cuda.mem_alloc(CS_f.nbytes)
-    I_f_gpu = cuda.mem_alloc(CS_f.nbytes)
-    hbar2_gpu  = cuda.mem_alloc(2 * CS_f.nbytes)
-    P_f_gpu = cuda.mem_alloc(2 * CS_f.nbytes)
+    R_f_gpu = cuda.mem_alloc(R_f.nbytes)
+    I_f_gpu = cuda.mem_alloc(I_f.nbytes)
+    hbar2_gpu  = cuda.mem_alloc(hbar2.nbytes)
+    P_f_gpu = cuda.mem_alloc(P_f.nbytes)
 
     # copy over samples..
     cuda.memcpy_htod(samples_gpu, samples)
@@ -155,14 +156,15 @@ def main():
     cuda.memcpy_htod(se_gpu, se_matrix_g)
     cuda.memcpy_htod(CS_f_gpu, CS_f_g)
     gpu_start = time.time()
+    bayes_gpu = mod.get_function('calc_bayes')
+    nsamples = np.int32(len(samples) / 2 / CUDA_GRID)
+    nalphas = np.int32(len(alfs))
+    dbar2 = np.float32((sum(np.real(samples) ** 2) + sum(np.imag(samples) ** 2)) / (nsamples))
+
     for i in range(100):
         # run kernel on GPU
-        bayes_gpu = mod.get_function('calc_bayes')
-        nsamples = np.int32(len(samples) / 2)
-        nalphas = np.int32(len(alfs))
-        dbar2 = np.float32((sum(np.real(samples) ** 2) + sum(np.imag(samples) ** 2)) / (nsamples))
-        bayes_gpu(samples_gpu, t_gpu, ce_gpu, se_gpu, CS_f_gpu, R_f_gpu, I_f_gpu, hbar2_gpu, P_f_gpu, np.float32(env_model), nsamples, nalphas, dbar2, block = (int(len(freqs)),1,1))
-
+        bayes_gpu(samples_gpu, t_gpu, ce_gpu, se_gpu, CS_f_gpu, R_f_gpu, I_f_gpu, hbar2_gpu, P_f_gpu, np.float32(env_model), nsamples, nalphas, dbar2, block = (int(len(freqs)),1,1), grid = (CUDA_GRID,1))
+    for i in range(1):
         # copy back data
         cuda.memcpy_dtoh(R_f, R_f_gpu) 
         cuda.memcpy_dtoh(I_f, I_f_gpu) 
@@ -171,13 +173,13 @@ def main():
 
     gpu_end = time.time()
 
-    #cpu_start = time.time()
-    #for i in range(10):
-    #    # run on CPU
-    #    R_f_cpu, I_f_cpu, hbar2_cpu, P_f_cpu = calculate_bayes(signal, lags, freqs, alfs, env_model, ce_matrix, se_matrix, CS_f)
+    cpu_start = time.time()
+    for i in range(7500):
+        R_f_cpu, I_f_cpu, hbar2_cpu, P_f_cpu = calculate_bayes(signal, lags, freqs, alfs, env_model, ce_matrix_cpu, se_matrix_cpu, CS_f_cpu)
 
-    #cpu_end = time.time()
+    cpu_end = time.time()
     print 'gpu: ' + str(gpu_end - gpu_start)
+    print 'cpu: ' + str(cpu_end - cpu_start)
     # compare..
 
 if __name__ == '__main__':
