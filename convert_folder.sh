ef9794d2c4fa2bc65c54c6d7c02a12bb68400686
Jon Klein <jtklein@alaska.edu>
2015-01-15 12:31:50 -0900

    removed obsoleted files

--- convert_folder.sh
+++
@@ -1,5 +0,0 @@
-# script to use GNU parallel to parallelize fitlomb calculations..
-
-find ~/rawdata/*.bz2 | parallel -j 6 'bunzip2 {}'
-find ~/rawdata/*.rawacf | parallel -j 6 'python2 rawacf_to_fitlomb.py --infile {}'
-
--- pydarn_convert.sh
+++
@@ -1,4 +0,0 @@
-# script to use GNU parallel to parallelize fitlomb calculations..
-
-cat radars | parallel -j 6 'python2 pydarn_fitlombgen.py --radar {}'
-
